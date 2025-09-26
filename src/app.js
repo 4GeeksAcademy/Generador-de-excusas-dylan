bd7af5566530f0a46c8cd517f110a279b422100c
dylanzephlyn-cmyk <dylan.zephlyn@hotmail.com>
2025-09-26 21:30:53 +0000

    generador de escusas termninado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let nouns = ["Mi loro ", "El repositorio ", "La election ", "El alcalde "];
+  let actions = ["dejo de repositear ", "le dio un patatus ", "se cayo por el balcon ", "se fue a otra dimension "];
+  let circumstances = ["y me quede loco ", "y aparecio Paul Pogba ", "y se apago la luz ", "y me quede sin sandwich "];
+  let when = ["cuando tenia que entregarle el proyecto a mi jefe.", "cuando el viernes pasado se me fue la hoya.", "cuando volvi del gimnasio.", "cuando mi hermana me dejo de hablar."];
+
+let parte1= Math. floor (Math. random() * nouns.length);
+let parte2= Math. floor (Math. random() * actions.length);
+let parte3= Math. floor (Math. random() * circumstances.length);
+let parte4= Math. floor (Math. random() * when.length);
+
+document.querySelector("#excusas").innerHTML = nouns[parte1] + actions[parte2] + circumstances[parte3] + when[parte4]
+}
+
