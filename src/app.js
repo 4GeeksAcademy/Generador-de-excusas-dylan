4b6d4d0834a23349a3d482a562d6b6a2cbf299fe
Dylan Steven Inchaurregui Dooley <dylan.zephlyn@hotmail.com>
2025-10-17 18:04:22 +0000

    correction de indentacion y buenas practicas

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   let nouns = ["Mi loro ", "El repositorio ", "La election ", "El alcalde "];
   let actions = ["dejo de repositear ", "le dio un patatus ", "se cayo por el balcon ", "se fue a otra dimension "];
   let circumstances = ["y me quede loco ", "y aparecio Paul Pogba ", "y se apago la luz ", "y me quede sin sandwich "];
   let when = ["cuando tenia que entregarle el proyecto a mi jefe.", "cuando el viernes pasado se me fue la hoya.", "cuando volvi del gimnasio.", "cuando mi hermana me dejo de hablar."];
 
-let parte1= Math. floor (Math. random() * nouns.length);
-let parte2= Math. floor (Math. random() * actions.length);
-let parte3= Math. floor (Math. random() * circumstances.length);
-let parte4= Math. floor (Math. random() * when.length);
+let startOfSentence = Math.floor(Math.random() * nouns.length);
+let middlePart1 = Math.floor(Math.random() * actions.length);
+let middlePart2 = Math.floor(Math.random() * circumstances.length);
+let endOfSentence = Math.floor(Math.random() * when.length);
+// modfificado nombre de variables y espacios inecesarios
 
-document.querySelector("#excusas").innerHTML = nouns[parte1] + actions[parte2] + circumstances[parte3] + when[parte4]
+document.querySelector("#excusas").innerHTML = nouns[startOfSentence] + actions[middlePart1] + circumstances[middlePart2] + when[endOfSentence]
 }
 
